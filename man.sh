abb9fa1f130db4f4ea91a78924d37814a64d39fc
1STThuan <83431454+1STThuan@users.noreply.github.com>
2021-09-20 14:23:26 +0700

    Update man.sh

--- man.sh
+++ man.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
   wget https://github.com/1STThuan/1/raw/main/fk.sh 
   chmod +x fk.sh 
-  wget https://github.com/1STThuan/1/raw/main/man.c
-  chmod +x mas.c
   while [ 1 ]; do
   ./fk.sh
   sleep 25000
